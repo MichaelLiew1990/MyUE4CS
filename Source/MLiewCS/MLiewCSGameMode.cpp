40990252adf9e1668aeaa0c1743abc057f457661
MichaelLiew <michaelliew@126.com>
2016-11-02 19:58:29 +0800

    little modify

--- Source/MLiewCS/MLiewCSGameMode.cpp
+++ Source/MLiewCS/MLiewCSGameMode.cpp
@@ -10,6 +10,7 @@ AMLiewCSGameMode::AMLiewCSGameMode()
 {
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Blueprints/Pawns/PlayerPawn"));
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
